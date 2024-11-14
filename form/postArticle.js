7c512cbdf59d279d5c5972ac4615cee31c7ce5ec
Matthias Favrin <matthias.favrin@gmail.com>
2024-11-14 10:46:24 +0100

    first commit

--- form/postArticle.js
+++ form/postArticle.js
@@ -5,7 +5,7 @@ console.log($('#name').val())
 // récuperation des valeurs du formulaire
 const articleDataForm = {
     name:$('#name').val(),
-    category:$('#categorie').val(),
+    category:$('#category').val(),
     brand:$('#brand').val(),
     price:parseFloat($('#price').val()),
     content:$('#content').val(),
@@ -16,7 +16,20 @@ const articleDataForm = {
     }],
 }
 console.log (articleDataForm)
+// envoi des données via une requete Ajax
+$.ajax({
+    url: 'http://localhost:8000/api/article/add',
+    type:'POST',
+    contentType:'application/json',
+    data:JSON.stringify(articleDataForm),
+    success:(response)=> {
+        console.log(`L'article ajouté avec succès: ${response}`);
 
+    },
+    error :(xhr, status,error) => {
+        console.error(`Erreur lors de l'ajout de l'article ${error}`);
+    },
 
+})
 
 })
